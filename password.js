caf51d04569957f381105adbbd5a4308c2c6d76d
Edwin <534694474@qq.com>
2023-10-09 22:45:55 +0800

    initial commit

---
+++ password.js
@@ -0,0 +1,8 @@
+loopy()   
+      function loopy() {   
+      var sWord =""  
+      while (sWord != "Edwin") {//设置密码
+      sWord = prompt("输入正确密码才能登陆!")   
+     }   
+	         alert("欢迎访问")   
+ }   
