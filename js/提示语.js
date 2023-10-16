bbb7e880250653cd851d2f1efdbecfc40e9c4505
Edwin <534694474@qq.com>
2023-10-16 21:42:41 +0800

    1.添加travel图片 2.修改sport文本 3.修改主页提示语

--- js/提示语.js
+++ js/提示语.js
@@ -1,6 +1,6 @@
 if (!sessionStorage.getItem('visited')) {
     // 如果标志不存在，显示欢迎消息
-    alert("欢迎来到Edwin的个人网页👏👏👏\n此网页是一个简易的静态网页\n采用HTML，CSS和少量的Javascript！\n之后我还会继续更新网站内容\n注：电脑端及iPad观看最佳（不同平台设计上还有点适配问题，新手出品大佬请见谅！）");
+    alert("欢迎来到Edwin的个人网页👏👏👏\n此网页是一个简易的静态网页\n采用HTML，CSS和少量的Javascript！\n之后我还会继续更新网站内容\n并且往后做的Java项目会以此主页作为入口，敬请期待！\n注：不同平台设计上仍需要改进分辨率适配问题\n新手出品大佬请见谅！");
     // 将标志存储到会话存储中，以表示用户已经访问过页面
     sessionStorage.setItem('visited', 'true');
 }
