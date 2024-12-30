b735d1b77694c206a6eb9ed5794a128ed0a38bf2
EdwinJunyu <H1111236_Chen>
2024-12-29 22:55:28 -0800

    修改提示语；添加更多网页；修改页脚样式

--- js/提示语.js
+++ js/提示语.js
@@ -1,7 +1,6 @@
 if (!sessionStorage.getItem('visited')) {
     // 如果标志不存在，显示欢迎消息
-    alert("欢迎来到Edwin的个人网页👏👏👏\n此网页是一个简易的静态网页\n采用HTML，CSS和少量的Javascript！\n之后我还会继续更新网站内容\n并且往后做的Java项目会以此主页作为入口，敬请期待！\n注：不同平台设计上仍需要改进分辨率适配问题\n新手出品大佬请见谅！");
-    alert("Welcome to Edwin's personal webpage! 👏👏👏\nThis is a simple static website, made with HTML, CSS, and a bit of JavaScript!\nI will continue to update the content of this site, and future Java projects will be accessible through this homepage, so stay tuned!\nNote: The design still needs improvement for resolution adaptation across different platforms.\nPlease excuse any shortcomings, as this is a beginner's work!");
+    alert("欢迎来到Edwin的个人网页👏👏👏\n此网页是一个简易的静态网页\n采用HTML，CSS和少量的Javascript！\n之后我还会继续更新网站内容\n并且往后做的Java项目会以此主页作为入口，敬请期待！\n注：不同平台设计上仍需要改进分辨率适配问题\n新手出品大佬请见谅！\nWelcome to Edwin's personal webpage 👏👏👏\nThis webpage is a simple static page,\nbuilt with HTML, CSS, and a little bit of JavaScript!\nI will continue updating the site content in the future,\nand upcoming Java projects will connect this homepage as one of the entry point. Stay tuned!\nNote: Resolution adaptation for different platforms still needs improvement.\nCreated by a beginner—thank you for your understanding!");
     // 将标志存储到会话存储中，以表示用户已经访问过页面
     sessionStorage.setItem('visited', 'true');
 }
